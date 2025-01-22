9eb3c2ddf25efa008abb5f5d2f2967e6caf28637
Ted-star <kibuthuteddy@gmail.com>
2025-01-23 00:20:28 +0300

    inv

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InventoryComponent } from './inventory/inventory.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'inventory', component: InventoryComponent}
+];
